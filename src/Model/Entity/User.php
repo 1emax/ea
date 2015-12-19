9591ec6323fdc3d1750994725cbca0ffa6bbc28c
LEmax <lemaxe@gmail.com>
2015-12-19 08:12:18 +0200

    add password hashing

--- src/Model/Entity/User.php
+++ src/Model/Entity/User.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Model\Entity;
 
+use Cake\Auth\DefaultPasswordHasher;
 use Cake\ORM\Entity;
 
 /**
@@ -34,4 +35,12 @@ class User extends Entity
         '*' => true,
         'id' => false,
     ];
+
+    // Code from bake.
+
+    protected function _setPassword($value)
+    {
+        $hasher = new DefaultPasswordHasher();
+        return $hasher->hash($value);
+    }
 }
